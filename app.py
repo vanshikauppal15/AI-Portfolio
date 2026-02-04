5bf88498a81f4a105f202deb7c18464363ef0730
vanshika15 <uppalvanshika2005@gmail.com>
2026-02-04 11:32:46 +0530

    Changes in app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
 
-app = Flask(__name__)    
+from flask import Flask, render_template
+app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome to the AI Portfolio App!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 
